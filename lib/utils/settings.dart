730186b3e576f721174fdb36af09ba4da57b5f12
linnminhtet23@gmail.com <linnminhtet23@gmail.com>
2022-05-05 20:00:11 -0700

    initial commit

---
+++ lib/utils/settings.dart
@@ -0,0 +1,3 @@
+const appId = 'e9837b681d7a497e9aeb8d7e376f5e0a';
+const token =
+    '006e9837b681d7a497e9aeb8d7e376f5e0aIACA3KZ9F+dGD11ExlzWqb+nYaS/3fbG9fbtV/WPx+ytPWBXiZ8AAAAAEAC3KIpZE8pyYgEAAQAMynJi';
